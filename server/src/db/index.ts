a19cda5b6a5c2124b9f7003568911cd04f073b2d
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-06-03 12:28:21 +0530

    "Updated .env file, package.json, and server code to include PrismaDB and modify register route and view"

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,12 +1,22 @@
 import express from "express";
+import { prismadb } from "../utils/prisma";
 
 const router = express.Router();
 
 router.use(express.json());
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
   console.log("reqest: ", req.body);
-  res.json({ received: req.body });
+  const resp = await prismadb.user.create({
+    data: {
+      email: "dsf",
+      name: "sdf",
+      password: "sdf",
+    },
+  });
+  console.log("data saved :", resp);
+
+  res.json({ received: resp });
 });
 
 export default router;
