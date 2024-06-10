cc9851bf11e73a02cb59c409f1d336965bb67a0a
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-06-10 15:11:03 +0530

    "Moved script from dashboard.ejs to dashboard.js, added jQuery script tag, and modified add project button functionality"

--- server/src/views/dashboard.js
+++ server/src/views/dashboard.js
@@ -1,1 +1,18 @@
-console.log("is the script running", baseUrl);
+// run script at html load
+
+const token = localStorage.getItem("token");
+if (!token) window.location.href = "/auth/login";
+(async () => {
+  const res = await fetch(`${baseUrl}/a/userfind`, {
+    headers: { authorization: `Bearer ${token}` },
+  });
+  if (res.status != 200) {
+    localStorage.removeItem("token");
+    window.location.href = "/auth/login";
+    return;
+  }
+  const json = await res.json();
+
+  document.getElementById("givenUsername").innerText = json.username;
+})();
+// run script at html load
