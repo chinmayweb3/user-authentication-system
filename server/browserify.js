d0c41bbacf23a0c0f5026a193755272f277c38df
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-06-13 22:20:28 +0530

    "Update browserify script to accept single argument and modify output file path"

--- server/browserify.js
+++ server/browserify.js
@@ -2,19 +2,14 @@ const { exec } = require("child_process");
 
 const args = process.argv.slice(2);
 
-const findArgIndex = (arg) => args.findIndex((a) => a === arg);
-
-console.log("args", args, args[1]);
-
-if (args.length !== 3 || findArgIndex("-o") == -1 || args[1] !== "-o") {
+if (args.length !== 1) {
   console.error("arguments are wrong or missing");
   process.exit(1);
 }
 
 exec(
-  `npx browserify src/views/${args[0]} -o src/views/${args[2]} -t [ babelify --presets [ @babel/preset-env ] ]`,
+  `npx browserify src/script/${args[0]}.js -o src/script/${args[0]}out.js -t [ babelify --presets [ @babel/preset-env ] ]`,
   (error, stdout, stderr) => {
-    console.log("all the end ", error, stdout, stderr);
     if (error) {
       console.error(`Error: ${error.message}`);
       return;
@@ -23,6 +18,6 @@ exec(
       console.error(`stderr: ${stderr}`);
       return;
     }
-    console.log(`successful. Output file: ${args[2]}`);
+    console.log(`successful. Output file: src/script/${args[0]}out.js`);
   }
 );
