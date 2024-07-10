f4131c95b7a6bcc766be46efb7a9d997c3156c85
chinmayweb3 <144161893+chinmayweb3@users.noreply.github.com>
2024-07-10 14:21:16 +0530

    remove unwanted files

--- packages/src/react/hooks.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const useSession = () => {
-  return <div>useSession</div>;
-};
-
-export default useSession;
--- packages/src/react/logout.tsx
+++
@@ -1,5 +0,0 @@
-const AuthLogout = () => {
-  return 0;
-};
-
-export default AuthLogout;
